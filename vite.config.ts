a0ef1f2861749eaff06a389a55bde364f28b03f8
Gurucharan87318 <gurucharan87418@gmail.com>
2026-03-25 17:52:14 +0530

    fix: add base path for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "./",   // ← only change
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
